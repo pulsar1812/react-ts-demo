50b69773d3ae3cfae34bf24ff231ea2ffa004fd1
pulsar1812 <cluster000@gmail.com>
2022-08-02 22:46:58 +0800

    Add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/Greet';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
+import { Container } from './components/Container';
 
 function App() {
+  const personName = {
+    first: 'Jon',
+    last: 'Snow',
+  };
+
+  const nameList = [
+    {
+      first: 'Jane',
+      last: 'Lee',
+    },
+    {
+      first: 'May',
+      last: 'Chan',
+    },
+    {
+      first: 'Jim',
+      last: 'Woo',
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Greet name='Jon' messageCount={10} isLoggedIn={true} />
+      <Greet name='Jon' isLoggedIn={true} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
+      <Status status='success' />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Jon Snow!</Heading>
+      </Oscar>
+      <Button
+        handleClick={(event, id) => {
+          console.log('Button clicked', event, id);
+        }}
+      />
+      <Input value='' handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: '1px solid black', padding: '1rem' }} />
     </div>
   );
 }

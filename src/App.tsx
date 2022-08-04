cad679882d24272e93621d53e2a934a346c86f2b
pulsar1812 <cluster000@gmail.com>
2022-08-04 23:34:11 +0800

    Add UserContext

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import { Status } from './components/Status';
 import { Button } from './components/Button';
 import { Input } from './components/Input';
 import { Container } from './components/Container';
+import { ThemeContextProvider } from './components/context/ThemeContext';
+import { Box } from './components/context/Box';
+import { UserContextProvider } from './components/context/UserContext';
+import { User } from './components/context/User';
 
 function App() {
   const personName = {
@@ -48,6 +52,12 @@ function App() {
       />
       <Input value='' handleChange={(event) => console.log(event)} />
       <Container styles={{ border: '1px solid black', padding: '1rem' }} />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
     </div>
   );
 }
